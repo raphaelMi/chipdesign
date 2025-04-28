9fe155087540b8f0d0c64ebc222b7deeab7e2687
Your Name <jan.auer@freenet.de>
2025-04-28 15:04:55 +0200

    Test

--- src/algorithm.cpp
+++ src/algorithm.cpp
@@ -2,7 +2,7 @@
 #include "algorithm.h"
 
 namespace Algorithm {
-
+//Test Test
 Coordinate dijkstra_steiner(std::vector<Terminal> const& terminals)
 {
   std::cerr << "Algorithm not implemented, returning number of terminals" << std::endl;
